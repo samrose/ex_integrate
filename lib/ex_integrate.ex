d6dbc785be644e69c864dc46cfbd93626076ac4a
Garrett George <garrett.michael.george@gmail.com>
2021-10-16 16:19:57 -0500

    refactor: convert nested function to pipeline

--- lib/ex_integrate.ex
+++ lib/ex_integrate.ex
@@ -4,12 +4,16 @@ defmodule ExIntegrate do
   """
 
   def run_steps(file) do
-    steps = Jason.decode!(File.read!(file))
+    steps =
+      file
+      |> File.read!()
+      |> Jason.decode!()
+
     steps
     |> Map.get("steps")
-    |> Enum.each(fn x->
-        path = System.find_executable(x["command"])
-        Rambo.run(path,x["args"], log: true)
+    |> Enum.each(fn x ->
+      path = System.find_executable(x["command"])
+      Rambo.run(path, x["args"], log: true)
     end)
   end
 end
