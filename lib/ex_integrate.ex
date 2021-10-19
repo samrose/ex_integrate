94dad76864a44141c029dc154b532316e4fb0647
Garrett George <garrett.michael.george@gmail.com>
2021-10-19 17:41:00 -0500

    refactor: rename config_json -> config

--- lib/ex_integrate.ex
+++ lib/ex_integrate.ex
@@ -8,12 +8,12 @@ defmodule ExIntegrate do
 
   @spec run_steps(filename :: binary) :: :ok
   def run_steps(filename) when is_binary(filename) do
-    config_json =
+    config =
       filename
       |> File.read!()
       |> Jason.decode!()
 
-    steps = Enum.map(config_json["steps"], &Step.new/1)
+    steps = Enum.map(config["steps"], &Step.new/1)
     Enum.each(steps, &run_step/1)
     :ok
   end
