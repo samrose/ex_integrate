3fa9d4df267a846819b97730e9d8177c61822190
Garrett George <garrett.michael.george@gmail.com>
2021-11-04 18:51:18 -0500

    style: sort aliases in ExIntegrate

--- lib/ex_integrate.ex
+++ lib/ex_integrate.ex
@@ -3,16 +3,15 @@ defmodule ExIntegrate do
   Documentation for `ExIntegrate`.
   """
 
+  alias ExIntegrate.Config
+  alias ExIntegrate.Pipeline
   alias ExIntegrate.Step
   alias ExIntegrate.StepRunner
-  alias ExIntegrate.Pipeline
-  alias ExIntegrate.Config
 
   @spec run_pipelines(filename :: binary) :: {:ok, Config.t()}
   def run_pipelines(filename) when is_binary(filename) do
-    %Config{} = config =
-      import_json(filename)
-      |> parse_config_params()
+    params = import_json(filename)
+    %Config{} = config = parse_config_params(params)
 
     Enum.each(config.pipelines, &Pipeline.run/1)
 
@@ -28,15 +27,7 @@ defmodule ExIntegrate do
       |> Jason.decode!()
   end
 
-  defp parse_config_params(config_params) do
-    pipelines =
-      config_params
-      |> Access.get("pipelines", [])
-      |> Enum.map(fn pipeline_attrs ->
-        steps = Enum.map(pipeline_attrs["steps"], &Step.new/1)
-        %Pipeline{steps: steps}
-      end)
-
-    %Config{pipelines: pipelines}
+  defp parse_config_params(params) do
+    Config.new(params)
   end
 end
