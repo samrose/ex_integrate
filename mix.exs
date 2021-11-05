ad954f23be19cc47a5a2ca2399435db1dfed293a
Garrett George <garrett.michael.george@gmail.com>
2021-11-05 09:34:36 -0500

    chore: add ExIntegrate application to mix.exs

--- mix.exs
+++ mix.exs
@@ -14,7 +14,8 @@ defmodule ExIntegrate.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {ExIntegrate.Application, []}
     ]
   end
 
