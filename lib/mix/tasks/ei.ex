ccf032fd892458fa7f5cca6398cc7c852a85c3d9
Garrett George <garrett.michael.george@gmail.com>
2021-11-10 09:58:28 -0600

    feat: add `mix ei` task (#13)

---
+++ lib/mix/tasks/ei.ex
@@ -0,0 +1,21 @@
+defmodule Mix.Tasks.Ei do
+  @moduledoc """
+  Runs an ExIntegrate build using the provided config file, or `ei.json` by default.
+  """
+
+  use Mix.Task
+
+  @default_config_path "ei.json"
+
+  @impl Mix.Task
+  def run(args) do
+    {parsed, _args, _opts} = parse_opts(args)
+    ei_config_file = Keyword.get(parsed, :config, @default_config_path)
+
+    ExIntegrate.run_steps(ei_config_file)
+  end
+
+  defp parse_opts(opts) do
+    OptionParser.parse(opts, switches: [config: :string])
+  end
+end
---
+++ test/mix/tasks/ei_test.exs
@@ -0,0 +1,11 @@
+defmodule Mix.Tasks.EiTest do
+  use ExUnit.Case, async: true
+
+  alias Mix.Tasks.Ei, as: EiTask
+
+  @ei_config_path "test/fixtures/ei.test.json"
+
+  test "runs an ExIntegrate build using the config file" do
+    assert EiTask.run(["--config", @ei_config_path]) == ExIntegrate.run_steps(@ei_config_path)
+  end
+end
