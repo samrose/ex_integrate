b4cabc006dac43b6c0fc3a5a9d28a35c343042a9
Garrett George <garrett.michael.george@gmail.com>
2021-11-11 07:38:14 -0600

    feat: construct, update, and check a run's pipeline graph

--- lib/ex_integrate/run.ex
+++ lib/ex_integrate/run.ex
@@ -2,22 +2,50 @@ defmodule ExIntegrate.Core.Run do
   alias ExIntegrate.Core.Pipeline
   alias ExIntegrate.Core.Step
 
-  defstruct [:pipelines]
+  @enforce_keys [:pipelines]
+  defstruct @enforce_keys ++ [:pipeline_graph]
 
   @type t :: %__MODULE__{
-          pipelines: [Pipeline.t()]
+          pipelines: [Pipeline.t()],
+          pipeline_graph: Graph.t()
         }
 
-  @spec new(attrs :: map) :: t()
-  def new(attrs) do
+  @spec new(params :: map) :: t()
+  def new(params) do
     pipelines =
-      attrs
-      |> Access.get("pipelines", [])
+      params
+      |> Access.fetch!("pipelines")
       |> Enum.map(fn pipeline_attrs ->
         steps = Enum.map(pipeline_attrs["steps"], &Step.new/1)
-        %Pipeline{steps: steps}
+        %Pipeline{name: pipeline_attrs["name"], steps: steps}
       end)
 
-    %__MODULE__{pipelines: pipelines}
+    pipeline_graph = set_up_pipeline_graph(pipelines)
+    struct!(__MODULE__, pipelines: pipelines, pipeline_graph: pipeline_graph)
+  end
+
+  # TODO: set up edges
+  defp set_up_pipeline_graph(pipelines) do
+    Graph.new(type: :directed)
+    |> Graph.add_vertices(pipelines)
+  end
+
+  @doc """
+  Updates the given pipeline in the run's collection.
+
+  Returns the run with updated pipeline.
+  """
+  @spec put_pipeline(t(), Pipeline.t(), Pipeline.t()) :: t()
+  def put_pipeline(%__MODULE__{} = run, %Pipeline{} = old_pipeline, %Pipeline{} = new_pipeline) do
+    updated_pipeline_graph = Graph.replace_vertex(run.pipeline_graph, old_pipeline, new_pipeline)
+    Map.put(run, :pipeline_graph, updated_pipeline_graph)
+  end
+
+  @doc """
+  Returns true if the pipeline is included in the run; otherwise, returns false.
+  """
+  @spec has_pipeline?(t(), Pipeline.t()) :: boolean
+  def has_pipeline?(%__MODULE__{} = run, %Pipeline{} = pipeline) do
+    Graph.has_vertex?(run.pipeline_graph, pipeline)
   end
 end
---
+++ test/ex_integrate/run_test.exs
@@ -0,0 +1,39 @@
+defmodule ExIntegrate.RunTest do
+  use ExUnit.Case, async: true
+
+  alias ExIntegrate.Core.Run
+  alias ExIntegrate.Core.Pipeline
+
+  @valid_params %{
+    "pipelines" => [
+      %{
+        "steps" => [
+          %{
+            "name" => "say hello",
+            "command" => "echo",
+            "args" => ["hello world!"]
+          }
+        ]
+      }
+    ]
+  }
+
+  test "update pipeline" do
+    assert %Run{} = run = Run.new(@valid_params)
+    assert pipeline = hd(run.pipelines)
+    updated_pipeline = %Pipeline{name: "updated pipeline", steps: []}
+
+    assert run = Run.put_pipeline(run, pipeline, updated_pipeline)
+
+    assert Run.has_pipeline?(run, updated_pipeline),
+           """
+           Expected pipeline graph:
+
+           #{inspect(run.pipeline_graph)}
+
+           to have pipeline:
+
+           #{inspect(pipeline)}.
+           """
+  end
+end
