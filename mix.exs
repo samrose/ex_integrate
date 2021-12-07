213fc0bdde1f3e0a4689039667ad5e92fa6d4c0e
samrose <samuel.rose@gmail.com>
2021-12-07 16:35:20 -0500

    updating respective files to use elixir 1.13.0 (#19)
    * updating respective files to use elixir 1.13.0
    
    * fix: attempting to resolve circleci
    
    * fix: attempting to resolve circleci

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExIntegrate.MixProject do
     [
       app: :ex_integrate,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
