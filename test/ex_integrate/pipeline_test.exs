4bad2267480c6b1befabcd982b50c6412c4f1719
Garrett George <garrett.michael.george@gmail.com>
2021-12-15 07:58:54 -0600

    fix: use proper constructors instead of struct literals

--- test/ex_integrate/pipeline_test.exs
+++ test/ex_integrate/pipeline_test.exs
@@ -4,23 +4,23 @@ defmodule ExIntegrate.PipelineTest do
   alias ExIntegrate.Core.{Pipeline, Step}
 
   test "get a pipeline's step by name" do
-    step = %Step{name: "my step", command: "foo", args: []}
-    pipeline = %Pipeline{name: "my pipeline", steps: [step]}
+    step = Step.new(name: "my step", command: "foo", args: [])
+    pipeline = Pipeline.new(name: "my pipeline", steps: [step])
 
     assert pipeline[step.name] == step
   end
 
   test "update a pipeline's step" do
-    step = %Step{name: "my step", command: "foo", args: []}
-    pipeline = %Pipeline{name: "my pipeline", steps: [step]}
+    step = Step.new(name: "my step", command: "foo", args: [])
+    pipeline = Pipeline.new(name: "my pipeline", steps: [step])
     updated_step = %{step | status_code: 0}
 
     assert Pipeline.put_step(pipeline, step, updated_step)
   end
 
   test "get and update a pipeline's step" do
-    step = %Step{name: "my step", command: "foo", args: []}
-    pipeline = %Pipeline{name: "my pipeline", steps: [step]}
+    step = Step.new(name: "my step", command: "foo", args: [])
+    pipeline = Pipeline.new(name: "my pipeline", steps: [step])
     updated_step = %{step | status_code: 0}
 
     assert Pipeline.get_and_update(pipeline, step.name, fn current_step ->
