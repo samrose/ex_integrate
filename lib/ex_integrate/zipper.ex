3c466a0a85e0520901f1fa843bc40728559032f3
Garrett George <garrett.michael.george@gmail.com>
2022-01-29 14:42:44 -0600

    refactor: convert Zipper internal representation from tuple to struct (#39)
    * feat: add type-checking function to Zipper
    
    * chore: clean up Zipper code
    
    * refactor: convert Zipper internal structure from tuple to struct
    
    * feat(zipper): replace a zipper's item at a given index
    
    * fix: typespec for Zipper constructor

--- lib/ex_integrate/zipper.ex
+++ lib/ex_integrate/zipper.ex
@@ -15,8 +15,26 @@ defmodule ExIntegrate.Core.Zipper do
     * [ElixirForum post](https://elixirforum.com/t/elixir-needs-a-fifo-type/5701/24)
   """
 
-  @opaque t :: {[term], term | nil | :end, [term]}
-  @opaque t(a_list) :: {[], nil, a_list}
+  @initial_position -1
+
+  @enforce_keys [:l, :r, :current, :size]
+  defstruct @enforce_keys ++ [position: @initial_position]
+
+  @opaque t :: %__MODULE__{
+            l: [term] | [],
+            current: term,
+            r: [term] | [],
+            size: non_neg_integer,
+            position: integer
+          }
+
+  @opaque t(val) :: %__MODULE__{
+            r: [val] | [],
+            l: [val] | [],
+            current: val | nil,
+            size: non_neg_integer,
+            position: integer
+          }
 
   defmodule TraversalError do
     defexception [:message]
@@ -28,55 +46,119 @@ defmodule ExIntegrate.Core.Zipper do
     end
   end
 
-  @spec zip(val) :: t(val) when val: list
-  def zip(list) when is_list(list) do
-    {[], nil, list}
+  defguard is_zipper(term) when is_struct(term, __MODULE__)
+
+  defguard is_at_start(zipper)
+           when zipper.l == [] and
+                  is_nil(zipper.current) and
+                  zipper.position == @initial_position
+
+  defguard is_at_end(zipper) when zipper.position == zipper.size
+
+  defguard is_out_of_bounds(zipper)
+           when is_zipper(zipper) and
+                  (zipper.position >= zipper.size or zipper.position < @initial_position)
+
+  @spec new([val]) :: t(val) when val: term
+  def new(list) when is_list(list) do
+    struct!(__MODULE__,
+      l: [],
+      current: nil,
+      r: list,
+      size: length(list),
+      position: @initial_position
+    )
   end
 
-  @spec node(t) :: term
-  def node({_l, current, _r}),
-    do: current
+  defdelegate zip(list), to: __MODULE__, as: :new
 
-  @spec right(t) :: t
-  def right({_, :end, []} = zipper),
+  @spec node(t) :: term
+  def node(%__MODULE__{} = zipper), do: zipper.current
+
+  @spec right(t) :: t | no_return
+  def right(zipper) when is_out_of_bounds(zipper),
     do: raise(TraversalError, zipper)
 
-  def right({l, current, []}),
-    do: right({l, current, [:end]})
+  def right(zipper) when is_at_start(zipper) do
+    zipper
+    |> Map.put(:current, hd(zipper.r))
+    |> Map.put(:r, tl(zipper.r))
+    |> Map.update(:position, nil, &(&1 + 1))
+  end
 
-  def right({[], nil, [head_r | tail_r]}),
-    do: {[], head_r, tail_r}
+  def right(%__MODULE__{r: []} = zipper) do
+    zipper
+    |> Map.put(:l, zipper.l ++ [zipper.current])
+    |> Map.update(:position, nil, &(&1 + 1))
+  end
 
-  def right({l, old_current, [head_r | tail_r]}) do
-    new_l = l ++ [old_current]
-    {new_l, head_r, tail_r}
+  def right(%__MODULE__{} = zipper) do
+    zipper
+    |> Map.put(:l, zipper.l ++ [zipper.current])
+    |> Map.put(:current, hd(zipper.r))
+    |> Map.put(:r, tl(zipper.r))
+    |> Map.update(:position, nil, &(&1 + 1))
+  end
+
+  @spec replace_at(t, non_neg_integer, term) :: t
+  def replace_at(%__MODULE__{size: size} = zipper, index, _)
+      when index >= size,
+      do: zipper
+
+  def replace_at(zipper, 0, new_value) when is_at_start(zipper) do
+    Map.update(zipper, :r, nil, fn r ->
+      List.replace_at(r, 0, new_value)
+    end)
+  end
+
+  def replace_at(%__MODULE__{position: position} = zipper, index, new_value)
+      when index == position do
+    Map.put(zipper, :current, new_value)
+  end
+
+  def replace_at(%__MODULE__{position: position} = zipper, index, new_value)
+      when index < position do
+    Map.update(zipper, :l, nil, fn l ->
+      List.replace_at(l, index, new_value)
+    end)
+  end
+
+  def replace_at(%__MODULE__{position: position} = zipper, index, new_value)
+      when index > position do
+    Map.update(zipper, :r, nil, fn r ->
+      r_index = index - position - 1
+      List.replace_at(r, r_index, new_value)
+    end)
   end
 
   @spec put_current(t, term) :: t
-  def put_current({l, _current_value, r}, new_value),
-    do: {l, new_value, r}
+  def put_current(%__MODULE__{} = zipper, new_value),
+    do: replace_at(zipper, zipper.position, new_value)
 
   @spec rightmost(t) :: term
-  def rightmost({_l, _current, r}),
-    do: List.last(r)
+  def rightmost(%__MODULE__{} = zipper),
+    do: zipper |> right_items() |> List.last()
 
-  def left_items({l, _current, _r}),
-    do: l
+  @spec left_items(t) :: list
+  def left_items(%__MODULE__{} = zipper), do: zipper.l
 
-  def right_items({_l, _current, r}),
-    do: r
+  @spec right_items(t) :: list
+  def right_items(%__MODULE__{} = zipper), do: zipper.r
 
-  def to_list({[], nil, r}),
-    do: r
+  @spec to_list(t) :: list
+  def to_list(zipper) when is_at_start(zipper), do: zipper.r
 
-  def to_list({l, current, r}),
-    do: l ++ [current | r]
+  def to_list(zipper) when is_at_start(zipper) or is_at_end(zipper),
+    do: zipper.l ++ zipper.r
+
+  def to_list(%__MODULE__{} = zipper),
+    do: zipper.l ++ [zipper.current] ++ zipper.r
 
   @spec end?(t) :: boolean
-  def end?(zipper) do
-    case zipper do
-      {_, :end, []} -> true
-      _ -> false
-    end
-  end
+  def end?(zipper) when is_at_end(zipper), do: true
+  def end?(_), do: false
+
+  @spec zipper?(term) :: boolean
+  def zipper?(zipper) when is_zipper(zipper), do: true
+  def zipper?(_), do: false
 end
--- test/ex_integrate/zipper_test.exs
+++ test/ex_integrate/zipper_test.exs
@@ -3,38 +3,93 @@ defmodule ExIntegrate.Core.ZipperTest do
 
   alias ExIntegrate.Core.Zipper, as: Z
 
-  test "initially, current item is nil" do
-    zipper = Z.zip([1, 2, :foo])
-    assert is_nil(Z.node(zipper))
+  describe "moving to the right" do
+    test "initially, current item is nil" do
+      zipper = Z.zip([1, 2, :foo])
+      assert is_nil(Z.node(zipper))
+    end
+
+    test "when there are still items to the right, moves to the right" do
+      zipper = [1, 2] |> Z.zip() |> Z.right()
+      assert Z.node(zipper) == 1
+    end
+
+    test "end position is one past the rightmost item" do
+      zipper = [1, 2] |> Z.zip() |> Z.right() |> Z.right() |> Z.right()
+      assert Z.end?(zipper)
+    end
+
+    test "raises when trying to move past the end" do
+      zipper = [1, 2] |> Z.zip()
+
+      assert_raise Z.TraversalError, fn ->
+        zipper |> Z.right() |> Z.right() |> Z.right() |> Z.right()
+      end
+    end
   end
 
-  describe "modifying current item" do
+  describe "updating an item" do
+    setup :generate_random_item
+
+    defp generate_random_item(_) do
+      item = Enum.random([:bar, [[42], "foo"], %{baz: :bat}])
+      [item: item]
+    end
+
+    test "returns updated zipper when zipper is at beginning", %{item: item} do
+      zipper =
+        [1, 2, 42]
+        |> Z.zip()
+        |> Z.replace_at(0, item)
+
+      assert Z.to_list(zipper) == [item, 2, 42]
+    end
+
+    test "returns updated zipper when item is in focus", %{item: item} do
+      zipper =
+        [1, 2, 42]
+        |> Z.zip()
+        |> Z.right()
+        |> Z.replace_at(0, item)
+
+      assert Z.to_list(zipper) == [item, 2, 42]
+    end
+
+    test "returns updated zipper when item is left of focus", %{item: item} do
+      zipper =
+        [1, 2, 42]
+        |> Z.zip()
+        |> Z.right()
+        |> Z.right()
+        |> Z.replace_at(0, item)
+
+      assert Z.to_list(zipper) == [item, 2, 42]
+    end
+
+    test "returns updated zipper when item is right of focus", %{item: item} do
+      zipper =
+        [1, 2, 42]
+        |> Z.zip()
+        |> Z.right()
+        |> Z.right()
+        |> Z.replace_at(2, item)
+
+      assert Z.to_list(zipper) == [1, 2, item]
+    end
+
+    test "returns original zipper when index is out of bounds" do
+      zipper = [1, 2, 42] |> Z.zip()
+      assert zipper == Z.replace_at(zipper, 100, :not_updated)
+    end
+  end
+
+  describe "updating current item" do
     test "on success, modifies the item" do
       zipper = [1, 2, 42] |> Z.zip() |> Z.right()
       new_item = Enum.random([:bar, [[42], "foo"], %{baz: :bat}])
       updated_zipper = Z.put_current(zipper, new_item)
 
       assert Z.node(updated_zipper) == new_item
-    end
-  end
-
-  describe "moving to the right" do
-    test "when there are items to the right, moves to the right" do
-      zipper = [1, 2] |> Z.zip() |> Z.right()
-      assert Z.node(zipper) == 1
-    end
-
-    test "when at the end, adds an 'end' token" do
-      zipper = [42, :foo, ["bar"]] |> Z.zip() |> Z.right() |> Z.right() |> Z.right() |> Z.right()
-      assert Z.node(zipper) == :end
-    end
-
-    test "raises when trying to move past the 'end' token" do
-      zipper = [1] |> Z.zip()
-
-      assert_raise Z.TraversalError, fn ->
-        zipper |> Z.right() |> Z.right() |> Z.right()
-      end
     end
   end
 
@@ -55,7 +110,11 @@ defmodule ExIntegrate.Core.ZipperTest do
 
   test "converts to list" do
     original_list = [1..2, {"something", :foo, []}, {{:bar}}]
+
     zipper = Z.zip(original_list)
+    assert Z.to_list(zipper) == original_list
+
+    zipper = zipper |> Z.right() |> Z.right() |> Z.right()
     assert Z.to_list(zipper) == original_list
   end
 
@@ -68,5 +127,17 @@ defmodule ExIntegrate.Core.ZipperTest do
       |> Z.right()
 
     assert Z.end?(zipper_at_end)
+  end
+
+  describe "checking if the input is a zipper" do
+    test "returns true for zippers" do
+      zipper = Z.zip([1, 2, 3])
+      assert Z.zipper?(zipper)
+    end
+
+    test "returns false for non-zippers" do
+      non_zipper = Enum.random([["not", "a", "zipper"], Enum.random(0..1000), :foobar])
+      refute Z.zipper?(non_zipper)
+    end
   end
 end
