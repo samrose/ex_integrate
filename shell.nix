b890dc1af4b235c926f1607316f170fd3bee6e1f
samrose <samuel.rose@gmail.com>
2022-02-07 16:58:18 -0500

    feat: reduce nix to using niv

--- shell.nix
+++ shell.nix
@@ -1,35 +1,18 @@
-{ pkgs ? import ./nixpkgs.nix {} }:
-
-with pkgs;
+{ sources ? import ./nix/sources.nix }:
 let
-  inherit (lib) optional optionals;
-
-  erlang = pkgs.beam.lib.callErlang ./erlang.nix {};
-
-  elixir = pkgs.beam.lib.callElixir ./elixir.nix {
-    inherit erlang;
-    debugInfo = true;
-  };
-
-  elixir-erlang-src = runCommand "elixir-erlang-src" {} ''
-    mkdir $out
-    ln -s ${elixir.src} $out/elixir
-    ln -s ${erlang.src} $out/otp
-  '';#used for vim configs
+  pkgs = import sources.nixpkgs { };
+  inputs = [
+    pkgs.cmake
+    pkgs.erlang
+    pkgs.elixir
+    pkgs.inotify-tools
+  ];
 in
 
-mkShell {
-  buildInputs = [ erlang elixir ]
-    ++ optional stdenv.isLinux inotify-tools
-    ++ optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
-      # For file_system on macOS.
-      CoreFoundation
-      CoreServices
-    ]);
-
+pkgs.mkShell {
+  buildInputs = inputs;
   shellHook = ''
     export LOCALE_ARCHIVE=/usr/lib/locale/locale-archive
   '';
 
 }
-
